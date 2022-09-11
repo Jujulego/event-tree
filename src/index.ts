f4469afb748da6599b70a5d81f885569376cbde2
Julien Capellari <julien.capellari@gmail.com>
2022-09-11 18:35:29 +0200

    feat: add waitForEvent util

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from './event';
 export * from './event-source';
 export * from './key';
 export * from './listener-tree';
-export * from './routing';
 export * from './utils';
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { EventGroupData, EventGroupKey, EventMap, EventObservable } from './event';
 import { JoinKey, Key, KeyPart, SplitKey } from './key';
 
 // Utils
@@ -7,4 +8,16 @@ export function splitKey<K extends Key>(key: K): SplitKey<K> {
 
 export function joinKey<S extends readonly KeyPart[]>(key: S): JoinKey<S> {
   return key.join('.') as JoinKey<S>;
+}
+
+export async function waitForEvent<M extends EventMap, PK extends EventGroupKey<M>>(
+  source: EventObservable<M>,
+  key: PK
+): Promise<EventGroupData<M, PK>> {
+  return new Promise((resolve) => {
+    const unsub = source.subscribe<PK>(key, (data: EventGroupData<M, PK>) => {
+      resolve(data);
+      unsub();
+    });
+  });
 }
