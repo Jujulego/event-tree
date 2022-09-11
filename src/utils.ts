7746a7bb28a36deaaf198412e3b382369bea5f6f
Julien Capellari <julien.capellari@gmail.com>
2022-09-12 00:38:28 +0200

    fix: joinKey handles partial keys

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,17 @@
 import { EventGroupData, EventGroupKey, EventMap, EventObservable } from './event';
-import { JoinKey, Key, KeyPart, SplitKey } from './key';
+import { JoinKey, Key, KeyPart, PartialKey, SplitKey } from './key';
 
 // Utils
 export function splitKey<K extends Key>(key: K): SplitKey<K> {
   return key.split('.') as SplitKey<K>;
 }
 
-export function joinKey<S extends readonly KeyPart[]>(key: S): JoinKey<S> {
-  return key.join('.') as JoinKey<S>;
+export function joinKey<S extends KeyPart[]>(...parts: S): JoinKey<S>;
+export function joinKey<P1 extends KeyPart, P2 extends KeyPart>(p1: P1, p2?: P2): `${P1}` | `${P1}.${P2}`;
+export function joinKey<P1 extends KeyPart, P2 extends KeyPart, P3 extends KeyPart>(p1: P1, p2?: P2, p3?: P3): `${P1}` | `${P1}.${P2}` | `${P1}.${P3}`;
+export function joinKey<P1 extends KeyPart, P2 extends KeyPart, P3 extends KeyPart, P4 extends KeyPart>(p1: P1, p2?: P2, p3?: P3, p4?: P4): `${P1}` | `${P1}.${P2}` | `${P1}.${P2}.${P3}` | `${P1}.${P2}.${P3}.${P4}`;
+export function joinKey(...parts: Array<KeyPart | undefined>): Key {
+  return parts.filter(p => p).join('.');
 }
 
 export async function waitForEvent<M extends EventMap, PK extends EventGroupKey<M>>(
