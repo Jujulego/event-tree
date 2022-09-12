737a7ffac73521f3e8efcfb576aa04988827cb08
Julien Capellari <julien.capellari@gmail.com>
2022-09-12 19:27:20 +0200

    feat: improve joinKey typing

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,34 @@
 import { EventGroupData, EventGroupKey, EventMap, EventObservable } from './event';
-import { JoinKey, Key, KeyPart, PartialKey, SplitKey } from './key';
+import { JoinKey, Key, KeyPart as KP, PartialKey, SplitKey } from './key';
 
 // Utils
 export function splitKey<K extends Key>(key: K): SplitKey<K> {
   return key.split('.') as SplitKey<K>;
 }
 
-export function joinKey<S extends KeyPart[]>(...parts: S): JoinKey<S>;
-export function joinKey<P1 extends KeyPart, P2 extends KeyPart>(p1: P1, p2?: P2): `${P1}` | `${P1}.${P2}`;
-export function joinKey<P1 extends KeyPart, P2 extends KeyPart, P3 extends KeyPart>(p1: P1, p2?: P2, p3?: P3): `${P1}` | `${P1}.${P2}` | `${P1}.${P2}.${P3}`;
-export function joinKey<P1 extends KeyPart, P2 extends KeyPart, P3 extends KeyPart, P4 extends KeyPart>(p1: P1, p2?: P2, p3?: P3, p4?: P4): `${P1}` | `${P1}.${P2}` | `${P1}.${P2}.${P3}` | `${P1}.${P2}.${P3}.${P4}`;
-export function joinKey(...parts: Array<KeyPart | undefined>): Key {
-  return parts.filter(p => p).join('.');
+export function joinKey<S extends [KP, ...KP[]]>(...parts: S): JoinKey<S>;
+export function joinKey<P1 extends KP, P2 extends KP>(p1: P1, p2?: P2): PartialKey<`${P1}.${P2}`>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP>(p1: P1, p2: P2, p3?: P3): JoinKey<[P1, PartialKey<`${P2}.${P3}`>]>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP>(p1: P1, p2?: P2, p3?: P3): PartialKey<`${P1}.${P2}.${P3}`>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP>(p1: P1, p2: P2, p3: P3, p4?: P4): JoinKey<[P1, P2, PartialKey<`${P3}.${P4}`>]>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP>(p1: P1, p2: P2, p3?: P3, p4?: P4): JoinKey<[P1, PartialKey<`${P2}.${P3}.${P4}`>]>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP>(p1: P1, p2?: P2, p3?: P3, p4?: P4): PartialKey<`${P1}.${P2}.${P3}.${P4}`>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP, P5 extends KP>(p1: P1, p2: P2, p3: P3, p4: P4, p5?: P5): JoinKey<[P1, P2, P3, PartialKey<`${P4}.${P5}`>]>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP, P5 extends KP>(p1: P1, p2: P2, p3: P3, p4?: P4, p5?: P5): JoinKey<[P1, P2, PartialKey<`${P3}.${P4}.${P5}`>]>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP, P5 extends KP>(p1: P1, p2: P2, p3?: P3, p4?: P4, p5?: P5): JoinKey<[P1, PartialKey<`${P2}.${P3}.${P4}.${P5}`>]>;
+export function joinKey<P1 extends KP, P2 extends KP, P3 extends KP, P4 extends KP, P5 extends KP>(p1: P1, p2?: P2, p3?: P3, p4?: P4, p5?: P5): PartialKey<`${P1}.${P2}.${P3}.${P4}.${P5}`>;
+export function joinKey(...parts: Array<KP | undefined>): Key {
+  const key: KP[] = [];
+
+  for (const part of parts) {
+    if (part === undefined) {
+      break;
+    }
+
+    key.push(part);
+  }
+
+  return key.join('.');
 }
 
 export async function waitForEvent<M extends EventMap, PK extends EventGroupKey<M>>(
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,4 +1,4 @@
-import { EventSource, waitForEvent } from '../src';
+import { EventSource, joinKey, splitKey, waitForEvent } from '../src';
 
 // Types
 type TestEventMap = {
@@ -15,6 +15,25 @@ beforeEach(() => {
 });
 
 // Tests
+describe('splitKey', () => {
+  it('should divide key in parts', () => {
+    expect(splitKey('a.b.c'))
+      .toEqual(['a', 'b', 'c']);
+  });
+});
+
+describe('joinKey', () => {
+  it('should join key parts', () => {
+    expect(joinKey('a', 'b', 'c'))
+      .toBe('a.b.c');
+  });
+
+  it('should join key parts until undefined', () => {
+    expect(joinKey('a', 'b', undefined, 'd', 'e'))
+      .toBe('a.b');
+  });
+});
+
 describe('waitForEvent', () => {
   it('should resolve when event is emitted', async () => {
     setTimeout(() => source.emit('success', true), 0);
