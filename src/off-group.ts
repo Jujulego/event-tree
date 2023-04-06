e23a05269bcd06763c50bdc1cd46cc02017e5464
Julien Capellari <julien.capellari@neoxia.com>
2023-04-06 16:42:45 +0200

    feat: simplify offGroup

--- src/off-group.ts
+++ src/off-group.ts
@@ -5,40 +5,17 @@ export interface OffGroup extends OffFn {
   add(fn: OffFn): void;
 }
 
-// Utils
-function isOffGroup(off: OffFn): off is OffGroup {
-  return 'add' in off;
-}
-
 export function offGroup(...offs: OffFn[]): OffGroup {
   const fns = new Set<OffFn>(offs);
   
   // Create group
   const off = () => {
     for (const fn of fns) {
-      fns.delete(fn);
       fn();
     }
   };
   
-  const group = Object.assign(off, {
-    add: (fn: OffFn) => {
-      const has = fns.has(fn);
-
-      fns.add(fn);
-
-      if (!has && isOffGroup(fn)) {
-        fn.add(group);
-      }
-    },
+  return Object.assign(off, {
+    add: (fn: OffFn) => fns.add(fn),
   });
-
-  // Add group in other groups
-  for (const fn of offs) {
-    if (isOffGroup(fn)) {
-      fn.add(group);
-    }
-  }
-
-  return group;
 }
