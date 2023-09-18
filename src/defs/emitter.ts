729f91954fac95dfec9e8de795ff1e21e2903da2
Julien Capellari <julien.capellari@gmail.com>
2023-09-18 23:05:50 +0200

    feat: add EmitValue type util

--- src/defs/emitter.ts
+++ src/defs/emitter.ts
@@ -1,10 +1,13 @@
 /**
  * Objects emitting events
  */
-export interface Emitter<D> {
+export interface Emitter<D = unknown> {
   /**
    * Emits event
    * @param data
    */
   next(data: D): void;
 }
+
+// Utils
+export type EmitValue<E extends Emitter> = E extends Emitter<infer D> ? D : never;
